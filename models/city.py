1d6497477777b27056b13980ee3917bad93cc27a
Abazspecial <abazspecial@gmail.com>
2023-11-13 03:00:29 +0100

    Ab

--- models/city.py
+++ models/city.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
-""" this is the City Module for HBNB project """
+"""
+Defines the City class
+"""
 from models.base_model import BaseModel
 
 
 class City(BaseModel):
-    """City class, contains state ID and name """
+    """Represent a city
+
+    Attributes:
+        state_id (str): The state id.
+        name (str): The name of the city
+
+    """
+
     state_id = ""
     name = ""
